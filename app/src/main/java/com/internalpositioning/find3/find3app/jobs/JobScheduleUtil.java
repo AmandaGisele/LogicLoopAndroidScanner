7ed0674c28de8cbea8a7ad75db6721dc5158efd6
Chris Soberon <chris.soberon.w5916@student.hogent.be>
2019-11-19 01:20:22 +0100

    [Fix] concurrency issues

--- app/src/main/java/com/internalpositioning/find3/find3app/jobs/JobScheduleUtil.java
+++ app/src/main/java/com/internalpositioning/find3/find3app/jobs/JobScheduleUtil.java
@@ -1,4 +1,4 @@
-package com.internalpositioning.find3.find3app;
+package com.internalpositioning.find3.find3app.jobs;
 
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
@@ -6,15 +6,17 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.os.PersistableBundle;
 
+import com.internalpositioning.find3.find3app.services.PositionUpdateService;
+
+import java.util.concurrent.TimeUnit;
+
 public class JobScheduleUtil {
 
-    // schedule the start of the service every 1 - 3 seconds
     public static void scheduleJob(Context context, PersistableBundle extrasBundle) {
-        ComponentName serviceComponent = new ComponentName(context, ScanService.class);
+        ComponentName serviceComponent = new ComponentName(context, PositionUpdateService.class);
         JobInfo.Builder builder = new JobInfo.Builder(0, serviceComponent);
 
-        builder.setMinimumLatency(1 * 1000); // wait at least
-        builder.setOverrideDeadline(3 * 1000); // maximum delay
+        builder.setPeriodic(TimeUnit.SECONDS.toMillis(5));
         //builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED); // require unmetered network
         builder.setRequiresDeviceIdle(false); // device should be idle
         builder.setRequiresCharging(false); // we don't care if the device is charging or not
@@ -25,5 +27,10 @@ public class JobScheduleUtil {
                 .build();
 
         jobScheduler.schedule(builder.build());
+    }
+
+    public static void cancelAll(Context context) {
+        JobScheduler jobScheduler = context.getSystemService(JobScheduler.class);
+        jobScheduler.cancelAll();
     }
 }
--- app/src/main/java/com/internalpositioning/find3/find3app/transitions/TransitionRecognitionReceiver.kt
+++ app/src/main/java/com/internalpositioning/find3/find3app/transitions/TransitionRecognitionReceiver.kt
@@ -1,27 +1,48 @@
-package com.internalpositioning.find3.find3app
+package com.internalpositioning.find3.find3app.transitions
 
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.PersistableBundle
+import android.support.v4.app.NotificationCompat
 import android.util.Log
 import com.google.android.gms.location.ActivityTransitionEvent
 import com.google.android.gms.location.ActivityTransitionResult
 import com.google.android.gms.location.DetectedActivity
+import com.internalpositioning.find3.find3app.MainActivity
+import com.internalpositioning.find3.find3app.R
+import com.internalpositioning.find3.find3app.jobs.JobScheduleUtil
+import com.internalpositioning.find3.find3app.util.BundleUtil
 
 class TransitionRecognitionReceiver : BroadcastReceiver() {
+    companion object {
+        val INTENT_ACTION = "com.internalpositioning.find3.find3app.ACTION_PROCESS_ACTIVITY_TRANSITIONS"
+    }
+
+    private val TAG = TransitionRecognitionReceiver::class.java!!.getSimpleName()
 
     lateinit var mContext: Context
+    private var bundle: PersistableBundle? = null
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        mContext = context!!
+        Log.i("DEBUG", "Debugging could be better");
 
-        Log.d("TransitionReceiver", "onReceive")
+        if (intent != null && INTENT_ACTION.equals(intent.getAction())) {
+            mContext = context!!
 
-        if (ActivityTransitionResult.hasResult(intent)) {
-            var result = ActivityTransitionResult.extractResult(intent)
 
-            if (result != null) {
-                processTransitionResult(result)
+            Log.d("TransitionReceiver", "onReceive")
+            bundle = PersistableBundle(BundleUtil.toPersistableBundle(intent!!.getExtras()))
+
+
+
+            if (ActivityTransitionResult.hasResult(intent)) {
+                var result = ActivityTransitionResult.extractResult(intent)
+
+                if (result != null) {
+                    processTransitionResult(result)
+                }
             }
         }
     }
@@ -35,34 +56,17 @@ class TransitionRecognitionReceiver : BroadcastReceiver() {
 
     private fun onDetectedTransitionEvent(activity: ActivityTransitionEvent) {
         when (activity.activityType) {
-            DetectedActivity.ON_BICYCLE,
-            DetectedActivity.RUNNING,
-            DetectedActivity.WALKING ->
-                /*case DetectedActivity.TILTING:
-                case DetectedActivity.STILL:*/ {
-
-                saveTransition(activity)
+            DetectedActivity.STILL -> {
+                JobScheduleUtil.cancelAll(mContext)
             }
             else -> {
-
+                // Cancel running jobs on activity change.
+                // Basically prevent the job from running multiple times concurrently.
+                // There's probably a better way to do this but it's late...
+                JobScheduleUtil.cancelAll(mContext)
+                // Re-schedule job
+                JobScheduleUtil.scheduleJob(mContext, bundle)
             }
-        }
-    }
-
-    /**
-     * In this example we save in preferences, but is a bad way to do that.
-     * Is an Example, in a real app we have to save in database.
-     */
-    private fun saveTransition(activity: ActivityTransitionEvent){
-        // Save in Preferences
-        val sharedPref = mContext?.getSharedPreferences(
-                TransitionRecognitionUtils.SHARED_PREFERENCES_FILE_KEY_TRANSITIONS, Context.MODE_PRIVATE)
-
-        var previousTransitions = sharedPref.getString(TransitionRecognitionUtils.SHARED_PREFERENCES_KEY_TRANSITIONS, "")
-        with (sharedPref.edit()) {
-            val transitions =  previousTransitions + TransitionRecognitionUtils.createTranstionString(activity)
-            putString(TransitionRecognitionUtils.SHARED_PREFERENCES_KEY_TRANSITIONS, transitions)
-            commit()
         }
     }
 }
